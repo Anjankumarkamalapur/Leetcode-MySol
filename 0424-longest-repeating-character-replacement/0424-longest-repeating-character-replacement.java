fc78fb2ad9c2eab2428f2823bafa7200e5f95087
Anjan Kumar Kamalapur <139578011+Anjankumarkamalapur@users.noreply.github.com>
2025-02-22 12:22:53 +0530

    Time: 31 ms (26.25%), Space: 45.6 MB (7.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -9,7 +9,7 @@ class Solution {
         {
             hm.put(s.charAt(right),hm.getOrDefault(s.charAt(right),0)+1);
             maxFreq = Math.max(maxFreq, hm.get(s.charAt(right)));
-            if ((right - left + 1) - maxFreq > k) {
+            while ((right - left + 1) - maxFreq > k) {
                 char leftChar = s.charAt(left);
                 hm.put(leftChar, hm.get(leftChar) - 1);
                 if (hm.get(leftChar) == 0) {

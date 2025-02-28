4931ff50435fc2afd1ca4a02d2a93e55116831e4
Anjan Kumar Kamalapur <139578011+Anjankumarkamalapur@users.noreply.github.com>
2025-02-28 22:52:10 +0530

    Time: 9 ms (10.41%), Space: 45.3 MB (70.7%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,15 +2,20 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();    
         for (int a : asteroids) {
+            //adding all +ve elements 
             if (a > 0) {
                 stack.push(a);
-            } else {
+            } 
+            else {
+                //poping if collision occurs
                 while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(a)) {
                     stack.pop();
                 }
+                // adding all -ve elements
                 if (stack.isEmpty() || stack.peek() < 0)  {
                     stack.push(a);
                 }
+                //poping if we are geting same numeber from leftside and rightside(collsion)
                 else if (stack.peek() == Math.abs(a)) {
                     stack.pop();
                 }

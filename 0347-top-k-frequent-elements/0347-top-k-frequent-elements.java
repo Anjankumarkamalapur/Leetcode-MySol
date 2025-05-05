cad2a38837a6840c5bc6fc86a9bf7e417fe74d09
Anjan Kumar Kamalapur <139578011+Anjankumarkamalapur@users.noreply.github.com>
2025-05-05 23:31:35 +0530

    Time: 11 ms (92.63%), Space: 45.8 MB (84.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        // Count frequency of each number
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> hm= new HashMap<>();
+        for(int i:nums)
+        {
+            hm.put(i,hm.getOrDefault(i,0)+1);
         }
-        // Create buckets: index is frequency, each bucket stores list of numbers
-        List<Integer>[] bucket = new List[nums.length + 1];
-        for (int key : freqMap.keySet()) {
-            int freq = freqMap.get(key);
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
+        List<Integer>[] bucket=new List[nums.length+1];
+        for(int i:hm.keySet())
+        {
+            int frq=hm.get(i);
+            if(bucket[frq]==null)
+            {
+                bucket[frq]=new ArrayList<>();
             }
-            bucket[freq].add(key);
+            bucket[frq].add(i);
         }
-
-        // Collect top K frequent elements from the bucket
-        List<Integer> result = new ArrayList<>();
-        for (int i = nums.length; i >= 0 && result.size() < k; i--) {
-            if (bucket[i] != null) {
-                result.addAll(bucket[i]);
+        List<Integer> res=new ArrayList<>();
+        for(int i=nums.length; i>=0 && res.size()<k; i--)
+        {
+            if(bucket[i]!=null)
+            {
+                res.addAll(bucket[i]);
             }
         }
-
-        // Convert result to int array
-        int[] topK = new int[k];
-        for (int i = 0; i < k; i++) {
-            topK[i] = result.get(i);
+        int[] top=new int[k];
+        for(int i=0;i<k;i++)
+        {
+            top[i]=res.get(i);
         }
-
-        return topK;
+        return top;
     }
 }
